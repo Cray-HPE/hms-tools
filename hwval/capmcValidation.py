e724b09cbf257c6d0dddaf01ad3f2ce85b854a3f
Michael Jendrysik <michael.jendrysik@hpe.com>
2021-01-21 08:54:12 -0600

    Added ability to select individual tests for execution

--- hwval/capmcValidation.py
+++ hwval/capmcValidation.py
@@ -21,12 +21,14 @@ def getNid(xname):
 
     URL = "https://api-gw-service-nmn.local/apis/smd/hsm/v1/State/Components/" + xname
 
+    dbgPrint(dbgMed, "POST: %s %s" % (URL, getHeaders))
+
     r = requests.get(url = URL, headers = getHeaders)
 
-    dbgPrint(dbgMed, r.text)
+    dbgPrint(dbgMed, "Response: %s" % r.text)
 
     if r.status_code >= 300:
-        return -1
+        return 1
 
     comp = json.loads(r.text)
 
@@ -46,7 +48,7 @@ def get_power_cap_capabilities(xname):
     if nid < 0:
         printNotHealthy("get_power_cap_capabilities")
         printExtraHealth(xname, "Could not get nid")
-        return -1
+        return 1
 
     postHeaders = {
             'Authorization': 'Bearer %s' % auth_token,
@@ -60,9 +62,11 @@ def get_power_cap_capabilities(xname):
 
     URL = "https://api-gw-service-nmn.local/apis/capmc/capmc/v1/get_power_cap_capabilities"
 
+    dbgPrint(dbgMed, "POST: %s %s %s" % (URL, postHeaders, payload))
+
     r = requests.post(url = URL, headers = postHeaders, data = json.dumps(payload))
 
-    dbgPrint(dbgMed, r.text)
+    dbgPrint(dbgMed, "Response: %s" % r.text)
 
     if r.status_code >= 500:
         label = "CAPMC"
@@ -77,7 +81,7 @@ def get_power_cap_capabilities(xname):
     if r.status_code >= 300:
         printNotHealthy("get_power_cap_capabilities")
         printExtraHealth(label, msg)
-        return -1
+        return 1
 
     capInfo = json.loads(r.text)
 
@@ -96,7 +100,7 @@ def get_power_cap_capabilities(xname):
             printExtraHealth("min", capMin)
             printExtraHealth("max", capMax)
             printExtraHealth("supply", supply)
-            return -1
+            return 1
         else:
             capMax = supply
 
@@ -112,7 +116,7 @@ def get_power_cap(xname):
     if nid < 0:
         printNotHealthy("get_power_cap")
         printExtraHealth(xname, "Could not get nid")
-        return -1
+        return 1
 
     postHeaders = {
             'Authorization': 'Bearer %s' % auth_token,
@@ -126,9 +130,11 @@ def get_power_cap(xname):
 
     URL = "https://api-gw-service-nmn.local/apis/capmc/capmc/v1/get_power_cap"
 
+    dbgPrint(dbgMed, "POST: %s %s %s" % (URL, postHeaders, payload))
+
     r = requests.post(url = URL, headers = postHeaders, data = json.dumps(payload))
 
-    dbgPrint(dbgMed, r.text)
+    dbgPrint(dbgMed, "Response: %s" % r.text)
 
     if r.status_code >= 500:
         label = "CAPMC"
@@ -143,7 +149,7 @@ def get_power_cap(xname):
     if r.status_code >= 300:
         printNotHealthy("get_power_cap")
         printExtraHealth(label, msg)
-        return -1
+        return 1
 
     capInfo = json.loads(r.text)
 
@@ -152,14 +158,14 @@ def get_power_cap(xname):
     if err != 0:
         printNotHealthy("get_power_cap")
         printExtraHealth(xname, capInfo['nids'][0]['err_msg'])
-        return -1
+        return 1
 
     val = capInfo['nids'][0]['controls'][0]['val']
 
     if val is not None and val <= 0:
         printNotHealthy("get_power_cap")
         printExtraHealth("value", val)
-        return -1
+        return 1
 
     printOK("get_power_cap")
 
@@ -168,17 +174,17 @@ def get_power_cap(xname):
 def set_power_cap(xname):
     dbgPrint(dbgMed, "set_power_cap")
 
+    if capMax == 0:
+        printNotHealthy("set_power_cap")
+        printExtraHealth("Invalid max cap value", capMax)
+        return 1
+
     nid = getNid(xname)
 
     if nid < 0:
         printNotHealthy("set_power_cap")
         printExtraHealth(xname, "Could not get nid")
-        return -1
-
-    if capMax == 0:
-        printNotHealthy("set_power_cap")
-        printExtraHealth("Invalid max cap value", capMax)
-        return -1
+        return 1
 
     # Get and save original value
     postHeaders = {
@@ -193,9 +199,11 @@ def set_power_cap(xname):
 
     URL = "https://api-gw-service-nmn.local/apis/capmc/capmc/v1/get_power_cap"
 
+    dbgPrint(dbgMed, "POST: %s %s %s" % (URL, postHeaders, payload))
+
     r = requests.post(url = URL, headers = postHeaders, data = json.dumps(payload))
 
-    dbgPrint(dbgMed, r.text)
+    dbgPrint(dbgMed, "Response: %s" % r.text)
 
     if r.status_code >= 500:
         label = "CAPMC"
@@ -210,7 +218,7 @@ def set_power_cap(xname):
     if r.status_code >= 300:
         printNotHealthy("set_power_cap")
         printExtraHealth(label, msg)
-        return -1
+        return 1
 
     capInfo = json.loads(r.text)
 
@@ -219,20 +227,20 @@ def set_power_cap(xname):
     if err != 0:
         printNotHealthy("set_power_cap")
         printExtraHealth(xname, "Node not in the Ready state")
-        return -1
+        return 1
 
     origVal = capInfo['nids'][0]['controls'][0]['val']
 
     if origVal is not None and origVal <= 0:
         printNotHealthy("set_power_cap")
         printExtraHealth("value", origVal)
-        return -1
+        return 1
 
     # Verify in range
     if origVal is not None and (origVal < capMin or origVal > capMax):
         printNotHealthy("set_power_cap")
         printExtraHealth("value", origVal)
-        return -1
+        return 1
 
     # Set to (max - 10)
     postHeaders = {
@@ -249,9 +257,11 @@ def set_power_cap(xname):
 
     URL = "https://api-gw-service-nmn.local/apis/capmc/capmc/v1/set_power_cap"
 
+    dbgPrint(dbgMed, "POST: %s %s %s" % (URL, postHeaders, payload))
+
     r = requests.post(url = URL, headers = postHeaders, data = json.dumps(payload))
 
-    dbgPrint(dbgMed, r.text)
+    dbgPrint(dbgMed, "Response: %s" % r.text)
 
     if r.status_code >= 500:
         label = "CAPMC"
@@ -266,7 +276,7 @@ def set_power_cap(xname):
     if r.status_code >= 300:
         printNotHealthy("set_power_cap")
         printExtraHealth(label, msg)
-        return -1
+        return 1
 
     capInfo = json.loads(r.text)
 
@@ -275,7 +285,7 @@ def set_power_cap(xname):
     if err != 0:
         printNotHealthy("set_power_cap")
         printExtraHealth(xname, capInfo['nids'][0]['err_msg'])
-        return -1
+        return 1
 
     # Get
 
@@ -291,9 +301,11 @@ def set_power_cap(xname):
 
     URL = "https://api-gw-service-nmn.local/apis/capmc/capmc/v1/get_power_cap"
 
+    dbgPrint(dbgMed, "POST: %s %s %s" % (URL, postHeaders, payload))
+
     r = requests.post(url = URL, headers = postHeaders, data = json.dumps(payload))
 
-    dbgPrint(dbgMed, r.text)
+    dbgPrint(dbgMed, "Response: %s" % r.text)
 
     if r.status_code >= 500:
         label = "CAPMC"
@@ -308,7 +320,7 @@ def set_power_cap(xname):
     if r.status_code >= 300:
         printNotHealthy("set_power_cap")
         printExtraHealth(label, msg)
-        return -1
+        return 1
 
     capInfo = json.loads(r.text)
 
@@ -317,21 +329,21 @@ def set_power_cap(xname):
     if err != 0:
         printNotHealthy("set_power_cap")
         printExtraHealth(xname, "Node not in the Ready state")
-        return -1
+        return 1
 
     setVal = capInfo['nids'][0]['controls'][0]['val']
 
     if setVal is not None and setVal <= 0:
         printNotHealthy("set_power_cap")
         printExtraHealth("value", setVal)
-        return -1
+        return 1
 
     # Verify avg of min/max
     if setVal != capVal:
         printNotHealthy("set_power_cap")
         printExtraHealth("set value", setVal)
         printExtraHealth("expected value", capVal)
-        return -1
+        return 1
 
     # Set original value
 
@@ -352,9 +364,11 @@ def set_power_cap(xname):
 
     URL = "https://api-gw-service-nmn.local/apis/capmc/capmc/v1/set_power_cap"
 
+    dbgPrint(dbgMed, "POST: %s %s %s" % (URL, postHeaders, payload))
+
     r = requests.post(url = URL, headers = postHeaders, data = json.dumps(payload))
 
-    dbgPrint(dbgMed, r.text)
+    dbgPrint(dbgMed, "Response: %s" % r.text)
 
     if r.status_code >= 500:
         label = "CAPMC"
@@ -369,7 +383,7 @@ def set_power_cap(xname):
     if r.status_code >= 300:
         printNotHealthy("set_power_cap")
         printExtraHealth(label, msg)
-        return -1
+        return 1
 
     printOK("set_power_cap")
 
@@ -383,7 +397,7 @@ def get_node_energy(xname):
     if nid < 0:
         printNotHealthy("get_node_energy")
         printExtraHealth(xname, "Could not get nid")
-        return -1
+        return 1
 
     postHeaders = {
             'Authorization': 'Bearer %s' % auth_token,
@@ -403,9 +417,11 @@ def get_node_energy(xname):
 
     URL = "https://api-gw-service-nmn.local/apis/capmc/capmc/v1/get_node_energy"
 
+    dbgPrint(dbgMed, "POST: %s %s %s" % (URL, postHeaders, payload))
+
     r = requests.post(url = URL, headers = postHeaders, data = json.dumps(payload))
 
-    dbgPrint(dbgMed, r.text)
+    dbgPrint(dbgMed, "Response: %s" % r.text)
 
     label = ""
     msg = ""
@@ -423,7 +439,7 @@ def get_node_energy(xname):
     if r.status_code >= 300:
         printNotHealthy("get_node_energy")
         printExtraHealth(label, msg)
-        return -1
+        return 1
 
     energyInfo = json.loads(r.text)
     err = energyInfo['e']
@@ -431,14 +447,14 @@ def get_node_energy(xname):
     if err > 0:
         printNotHealthy("get_node_energy")
         printExtraHealth(xname, "No data in time window")
-        return -1
+        return 1
 
     energy = energyInfo['nodes'][0]['energy']
 
     if energy <= 0:
         printNotHealthy("get_node_energy")
         printExtraHealth("energy", energy)
-        return -1
+        return 1
 
     printOK("get_node_energy")
 
@@ -452,7 +468,7 @@ def get_node_energy_stats(xname):
     if nid < 0:
         printNotHealthy("get_node_energy_stats")
         printExtraHealth(xname, "Could not get nid")
-        return -1
+        return 1
 
     postHeaders = {
             'Authorization': 'Bearer %s' % auth_token,
@@ -472,9 +488,11 @@ def get_node_energy_stats(xname):
 
     URL = "https://api-gw-service-nmn.local/apis/capmc/capmc/v1/get_node_energy_stats"
 
+    dbgPrint(dbgMed, "POST: %s %s %s" % (URL, postHeaders, payload))
+
     r = requests.post(url = URL, headers = postHeaders, data = json.dumps(payload))
 
-    dbgPrint(dbgMed, r.text)
+    dbgPrint(dbgMed, "Response: %s" % r.text)
 
     label = ""
     msg = ""
@@ -492,7 +510,7 @@ def get_node_energy_stats(xname):
     if r.status_code >= 300:
         printNotHealthy("get_node_energy_stats")
         printExtraHealth(label, msg)
-        return -1
+        return 1
 
     energyInfo = json.loads(r.text)
     err = energyInfo['e']
@@ -500,14 +518,14 @@ def get_node_energy_stats(xname):
     if err > 0:
         printNotHealthy("get_node_energy_stats")
         printExtraHealth(xname, "No data in time window")
-        return -1
+        return 1
 
     energy = energyInfo['energy_total']
 
     if energy <= 0:
         printNotHealthy("get_node_energy_stats")
         printExtraHealth("energy_total", energy)
-        return -1
+        return 1
 
     printOK("get_node_energy_stats")
     return 0
@@ -520,7 +538,7 @@ def get_node_energy_counter(xname):
     if nid < 0:
         printNotHealthy("get_node_energy_stats")
         printExtraHealth(xname, "Could not get nid")
-        return -1
+        return 1
 
     postHeaders = {
             'Authorization': 'Bearer %s' % auth_token,
@@ -540,9 +558,11 @@ def get_node_energy_counter(xname):
 
     URL = "https://api-gw-service-nmn.local/apis/capmc/capmc/v1/get_node_energy_counter"
 
+    dbgPrint(dbgMed, "POST: %s %s %s" % (URL, postHeaders, payload))
+
     r = requests.post(url = URL, headers = postHeaders, data = json.dumps(payload))
 
-    dbgPrint(dbgMed, r.text)
+    dbgPrint(dbgMed, "Response: %s" % r.text)
 
     label = ""
     msg = ""
@@ -560,7 +580,7 @@ def get_node_energy_counter(xname):
     if r.status_code >= 300:
         printNotHealthy("get_node_energy_counter")
         printExtraHealth(label, msg)
-        return -1
+        return 1
 
     energyInfo = json.loads(r.text)
     err = energyInfo['e']
@@ -568,14 +588,14 @@ def get_node_energy_counter(xname):
     if err > 0:
         printNotHealthy("get_node_energy_counter")
         printExtraHealth(xname, "No data in time window")
-        return -1
+        return 1
 
     energy = energyInfo['nodes'][0]['energy_ctr']
 
     if energy <= 0:
         printNotHealthy("get_node_energy_counter")
         printExtraHealth("energy_ctr", energy)
-        return -1
+        return 1
 
     printOK("get_node_energy_counter")
     return 0
@@ -595,9 +615,11 @@ def get_system_power(xname):
 
     URL = "https://api-gw-service-nmn.local/apis/capmc/capmc/v1/get_system_power"
 
+    dbgPrint(dbgMed, "POST: %s %s %s" % (URL, postHeaders, payload))
+
     r = requests.post(url = URL, headers = postHeaders, data = json.dumps(payload))
 
-    dbgPrint(dbgMed, r.text)
+    dbgPrint(dbgMed, "Response: %s" % r.text)
 
     label = ""
     msg = ""
@@ -615,7 +637,7 @@ def get_system_power(xname):
     if r.status_code >= 300:
         printNotHealthy("get_system_power")
         printExtraHealth(label, msg)
-        return -1
+        return 1
 
     powerInfo = json.loads(r.text)
     err = powerInfo['e']
@@ -623,14 +645,14 @@ def get_system_power(xname):
     if err > 0:
         printNotHealthy("get_system_power")
         printExtraHealth("system", "No data in time window")
-        return -1
+        return 1
 
     avgPower = powerInfo['avg']
 
     if avgPower <= 0:
         printNotHealthy("get_system_power")
         printExtraHealth("Avg Power", avgPower)
-        return -1
+        return 1
 
     printOK("get_system_power")
     return 0
@@ -650,9 +672,11 @@ def get_system_power_details(xname):
 
     URL = "https://api-gw-service-nmn.local/apis/capmc/capmc/v1/get_system_power_details"
 
+    dbgPrint(dbgMed, "POST: %s %s %s" % (URL, postHeaders, payload))
+
     r = requests.post(url = URL, headers = postHeaders, data = json.dumps(payload))
 
-    dbgPrint(dbgMed, r.text)
+    dbgPrint(dbgMed, "Response: %s" % r.text)
 
     label = ""
     msg = ""
@@ -670,7 +694,7 @@ def get_system_power_details(xname):
     if r.status_code >= 300:
         printNotHealthy("get_system_power_details")
         printExtraHealth(label, msg)
-        return -1
+        return 1
 
     powerInfo = json.loads(r.text)
     err = powerInfo['e']
@@ -678,14 +702,14 @@ def get_system_power_details(xname):
     if err > 0:
         printNotHealthy("get_system_power_details")
         printExtraHealth("system", "No data in time window")
-        return -1
+        return 1
 
     avgPower = powerInfo['cabinets'][0]['avg']
 
     if avgPower <= 0:
         printNotHealthy("get_system_power_details")
         printExtraHealth("Avg Power", avgPower)
-        return -1
+        return 1
 
     printOK("get_system_power_details")
     return 0
@@ -705,9 +729,11 @@ def get_xname_status(xname):
 
     URL = "https://api-gw-service-nmn.local/apis/capmc/capmc/v1/get_xname_status"
 
+    dbgPrint(dbgMed, "POST: %s %s %s" % (URL, postHeaders, payload))
+
     r = requests.post(url = URL, headers = postHeaders, data = json.dumps(payload))
 
-    dbgPrint(dbgMed, r.text)
+    dbgPrint(dbgMed, "Response: %s" % r.text)
 
     label = ""
     msg = ""
@@ -725,7 +751,7 @@ def get_xname_status(xname):
     if r.status_code >= 300:
         printNotHealthy("get_xname_status")
         printExtraHealth(label, msg)
-        return -1
+        return 1
 
     status = json.loads(r.text)
     err = status['e']
@@ -733,7 +759,7 @@ def get_xname_status(xname):
     if err < 0:
         printNotHealthy("get_xname_status")
         printExtraHealth(xname, "Could not talk to BMC, undefined")
-        return -1
+        return 1
 
     printOK("get_xname_status")
 
@@ -751,16 +777,27 @@ validations = [
         get_xname_status
         ]
 
-def capmcValidation(xname):
+def capmcValidation(xname, tests=None, list=False):
     dbgPrint(dbgMed, "capmcValidation")
 
-    idx = 0
-    while idx < len(validations):
-        dbgPrint(dbgMed, "Calling: %d %s" % (idx, validations[idx].__name__))
-        validations[idx](xname)
-        idx = idx + 1
+    if list:
+        return validations
 
-    return 0
+    failures = 0
+    if tests:
+        for t in tests:
+            for test in validations:
+                if t == test.__name__:
+                    dbgPrint(dbgMed, "Calling: capmcValidation:%s" % test.__name__)
+                    ret = test(xname)
+                    failures = failures + ret
+    else:
+        for test in validations:
+            dbgPrint(dbgMed, "Calling: capmcValidation:%s" % test.__name__)
+            ret = test(xname)
+            failures = failures + ret
+
+    return failures
 
 if __name__ == "__main__":
     setDbgLevel(dbgLow)
